b8be8996244a6fe8989442bb4093e1f0368c6ba9
ccorrea <ccorrea@bidda.net>
2024-01-23 22:46:15 -0500

    webpack setup (checkpoint)

--- private/webpack.config.js
+++ private/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const glob = require('glob');
-
-let entries = {};
-// Busca todos los archivos .js en el directorio 'private/js/parts' y los agrega como puntos de entrada
-glob.sync('./private/js/**/*.js').forEach(filePath => {
-  let entry = filePath.replace('/private/js/', '');
-  entries[entry] = filePath;
-});
 
 module.exports = {
   mode: 'production',
 
   // Puntos de entrada de tu aplicación
-  entry: entries,
+  entry: {
+    // Utiliza el nombre del archivo relativo como clave y el path completo como valor
+    ...glob.sync('./private/js/parts/**/*.js').reduce((entries, filePath) => {
+      const newFilePath = filePath.replace('./private/js/', '');
+      entries[newFilePath] = path.resolve(__dirname, filePath);
+      return entries;
+    }, {}),
+  },
 
   // Configuración de salida
   output: {
-    filename: '[name].min.js',
+    filename: '[name]',
     path: path.resolve(__dirname, 'js'),
+    publicPath: '/js/', // Agrega esta línea para indicar la ruta pública de tus archivos generados
   },
 
   // Configuración de módulos y reglas
@@ -31,11 +31,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 
   // Configuración de optimización
@@ -48,12 +48,13 @@ module.exports = {
   resolve: {
     alias: {
       // Aquí puedes agregar alias para tus módulos
-    }
+    },
   },
 
   plugins: [
+    // Puedes habilitar el Bundle Analyzer si es necesario
     // new BundleAnalyzerPlugin()
-  ]
+  ],
 };
 
 // const path = require('path');
