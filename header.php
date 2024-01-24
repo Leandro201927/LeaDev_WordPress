5567798c68a88fc605d0b8e35e068b7bf77b787a
ccorrea <ccorrea@bidda.net>
2024-01-23 23:54:52 -0500

    header update

--- header.php
+++ header.php
@@ -44,7 +44,8 @@
 
   <body>
     <script>
-      (function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+      // Inspect remote devices
+      // (function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
     </script>
     <header>
       <div class="lock__ui">
